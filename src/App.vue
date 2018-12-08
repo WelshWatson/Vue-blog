85ed564d935a1d4e5bf7a5a59be83be9a7f7955a
WelshWatson <owen_watson84@hotmail.com>
2018-12-08 13:49:25 +0000

    Home and navbar setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar header="Welcome to Your Vue.js App"/>
+    <Home />
+        <img alt="Vue logo" src="./assets/logo.png">
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import Home from './components/Home';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Navbar,
+    Home
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
